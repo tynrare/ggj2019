2b1cf3830790dd829e42e4454f6980f62db6fafa
Tymonr <tymonr@yandex.ru>
2019-01-27 04:27:56 +0300

    dialogues

--- scripts/dialogue_scene.gd
+++ scripts/dialogue_scene.gd
@@ -14,11 +14,13 @@ func _ready():
 func update_scene():
 	#example
 	if gameModel.roundCounter == 0:
-		set_text("Welcome!\n 123?:?:;% тест")
-		make_dialogue_btns("first_test_di", ["Nope", "Yep", "SARKASM", "Getta out"]);
+		make_monologue_chain("first_test_di", ["Welcome!", "1", "2", "3", "what next?"]);
 
 func set_location(name : String):
 	get_node("back").texture = load("res://img/dialogues/locations/"+name+".jpg");
+
+func toggle_character_visible():
+	get_node("character").visible = !get_node("character").visible;
 
 func set_character(name : String):
 	get_node("character").texture = load("res://img/dialogues/characters/"+name+".png");
@@ -29,13 +31,43 @@ func set_text(text : String):
 func _dialogue_selected(frameId : String, selectId : int):
 	match frameId:
 		"first_test_di":
-			if selectId == 3:
+			#exit from chain
+			if selectId == -1:
+				make_dialogue_btns("first_test_di", ["Nope", "Yep", "SARKASM", "Getta out"]);
+			#selected exit 
+			elif selectId == 3:
 				get_node("..").enter_to_map();
+			#other
 			else:
 				set_text("Folk you");
 	print(frameId, selectId);
+
+var _monologue_chain_data = [];
+
+func make_monologue_chain(frameId : String, text : Array):
+	_monologue_chain_data = text;
+	_monologue_chain_next(frameId);
 	
+func _monologue_chain_next(frameId):
+	_remove_all_btns();
+	if !_monologue_chain_data.size():
+		_dialogue_selected(frameId, -1);
+		return;
+		
+	set_text(_monologue_chain_data.pop_front());
+	
+	var b = Button.new();
+	get_node("buttonsContainer").add_child(b);
+	b.connect("pressed", self, "_monologue_chain_next",[frameId])
+	b.text = "next";
+
+func _remove_all_btns():
+	for c in get_node("buttonsContainer").get_children():
+    	c.queue_free()
+
 func make_dialogue_btns(id : String, variants : Array):
+	_remove_all_btns();
+		
 	for el in variants:
 		var b = Button.new();
 		get_node("buttonsContainer").add_child(b);

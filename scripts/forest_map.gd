3ed13de43ec640cacb80aa7ada5d09511760fd1b
Tymonr <tymonr@yandex.ru>
2019-01-27 16:55:43 +0300

    fix

--- scripts/forest_map.gd
+++ scripts/forest_map.gd
@@ -61,7 +61,7 @@ func get_base_translation(card) -> Vector2:
 			"food":
 				return Vector2(1, math_model.turn);
 			_:
-				return Vector2(0, math_model.turn + 3); 
+				return Vector2(0, math_model.turn + 2); 
 	else:
 		match math_model.mapRegion:
 			"tutorial":
